4e1f9727fdfae0fec67204fd64fb26897478f3a3
scirin <irine.kokilashvili@gmail.com>
2023-03-19 16:37:45 +0000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use spider::configuration::Configuration;
-use spider::utils::CompactString;
 use spider::website::Website;
 use spider::tokio;
 use std::env;
@@ -13,19 +12,21 @@ async fn main() {
     let url = &args[1];
 
     let mut config = Configuration::new();
-    config.blacklist_url.expect("blacklist_url is None")
-        .push(format!("{}/licenses/", url).into());
+    if let Some(blacklist_url) = config.blacklist_url.as_ref() {
+        blacklist_url.push(format!("{}/licenses/", url).into());
+    }
     config.respect_robots_txt = true;
     config.subdomains = true;
     config.tld = false;
     config.delay = 0;
     config.request_timeout = None;
     config.channel_buffer = 100;
-    config.user_agent = "myapp/version".to_string();
+    config.user_agent = Some(Box::new("myapp/version".to_string().into()));
+    
+    let mut website: Website = Website::new(url);
+    website.configuration = Box::new(config);
 
-    let mut website: Website = Website::with_configuration(url, config);
-    website.crawl().await;
-
+    
     for link in website.get_links() {
         println!("- {:?}", link.as_ref());
     }
