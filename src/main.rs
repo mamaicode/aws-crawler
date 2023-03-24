98b9fe024b6ffcef26d268149f03d55833d80cf3
scirin <irine.kokilashvili@gmail.com>
2023-03-24 18:07:20 +0000

    [FEATURE] fix

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
+use std::env;
+use std::io::{self, Write};
+
+use aws_config::load_from_env;
+use aws_sdk_s3::types::SdkError;
+use aws_sdk_s3::types::ByteStream;
+use aws_sdk_s3::{Client as S3Client};
+
 use spider::configuration::Configuration;
+use spider::reqwest;
 use spider::website::Website;
 
-use tokio::{fs::File, io::AsyncReadExt};
-
-use std::{env, io::Read};
-
-use aws_sdk_s3::{ByteStream, Client, Region};
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
+
     if args.len() != 2 {
         panic!("Please provide a URL to crawl");
     }
+
     let url = &args[1];
 
     let mut config = Configuration::new();
@@ -26,14 +34,17 @@ async fn main() {
     config.request_timeout = None;
     config.channel_buffer = 100;
     config.user_agent = Some(Box::new("myapp/version".to_string().into()));
-  
+
     let mut website: Website = Website::new(url);
     website.configuration = Box::new(config);
 
-    // to fix
-    let s3_client = Client::new(Region::default());
+    println!("Enter the name of the S3 bucket to save the crawled data:");
+    io::stdout().flush()?;
+    let mut bucket_name = String::new();
+    io::stdin().read_line(&mut bucket_name)?;
 
-    // to fix
+    let s3_client = S3Client::new(&load_from_env().await?);
+
     for link in website.get_links() {
         println!("- {:?}", link.as_ref());
         let mut response = reqwest::get(link.as_ref()).await?;
@@ -43,8 +54,8 @@ async fn main() {
         let key = format!("{}{}", website.base_url(), link);
         let byte_stream = ByteStream::from(body);
         let put_request = s3_client.put_object()
-            .bucket(bucket_name)
-            .key(key)
+            .bucket(&*bucket_name.trim())
+            .key(&key)
             .body(byte_stream);
         put_request.send().await?;
     }
